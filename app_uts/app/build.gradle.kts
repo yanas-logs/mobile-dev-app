63239a62da1404ca6003e2fb6c280c2e9f5c408c
yanas-logs <81765889+yanas-logs@users.noreply.github.com>
2026-01-03 19:42:26 +0700

    Update targetSdk version to 36

--- app_uts/app/build.gradle.kts
+++ app_uts/app/build.gradle.kts
@@ -10,7 +10,7 @@ android {
     defaultConfig {
         applicationId = "com.example.app_uts"
         minSdk = 29
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
 
